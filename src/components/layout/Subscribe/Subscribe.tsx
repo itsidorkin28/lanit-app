9f0cd7709620d3820615e3048a961dfebd46f5f4
itsidorkin28 <itsidorkin28@gmail.com>
2022-08-19 03:50:33 +0300

    media

--- src/components/layout/Subscribe/Subscribe.tsx
+++ src/components/layout/Subscribe/Subscribe.tsx
@@ -6,7 +6,7 @@ export const Subscribe: FC = (): JSX.Element => {
 	return (
 		<section className={styles.subscribe}>
 			<PageTitle title={'Don’t miss anything'} description={'Subscribe to the Createx School announcements'} />
-			<form>
+			<form className={styles.form}>
 				<input type='email' placeholder={'Your working email'} className={styles.input}/>
 				<button type={'submit'} className={styles.btn}>Subscribe</button>
 			</form>
